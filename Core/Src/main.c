44cc3a46d98f69b5b7bfd5cc6c86a5180855dec2
OpinionThief <82518058+11-pog@users.noreply.github.com>
2025-05-02 11:13:39 -0300

    Feita função de detecção de butão (pode ser melhorado)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under BSD 3-Clause license,
-  * the "License"; You may not use this file except in compliance with the
-  * License. You may obtain a copy of the License at:
-  *                        opensource.org/licenses/BSD-3-Clause
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2025 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under BSD 3-Clause license,
+ * the "License"; You may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at:
+ *                        opensource.org/licenses/BSD-3-Clause
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -70,7 +70,17 @@ void ExibirFimDeJogo(void);
 
 // Button Detection
 void AwaitForAnyButton(void);
-char DetectButtonPress(uint16_t Buttons[]);
+void ReadButtons(char *out);
+void DetectAnyButtonPress(char *out);
+void DetectButtonPress(uint16_t buttons[], char *out);
+
+// Screens
+void Menu(void);
+void PrintSelectDifficulty(char SelectedColor);
+void SelectDifficulty(void);
+
+// Other/Util
+char Contain(char *Iterable, char Contains);
 
 // Pokemon
 char mander(void);
@@ -80,281 +90,306 @@ char cadet(void);
 int elion(void);
 float zel(void);
 
-
 // que(rosene)
 bool AAAAAAAA(void);
 int ernet(void);
-
-// Screens
-void Menu(void);
-void PrintSelectDifficulty(void);
-void SelectDifficulty(void);
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void) {
+	/* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+	/* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+	/* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+	HAL_Init();
 
-  /* USER CODE BEGIN Init */
+	/* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+	/* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+	/* Configure the system clock */
+	SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+	/* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+	/* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_SPI1_Init();
-  /* USER CODE BEGIN 2 */
-  ST7789_Init();
-  Menu();
-  AwaitForAnyButton();
-  SelectDifficulty();
-  /* USER CODE END 2 */
+	/* Initialize all configured peripherals */
+	MX_GPIO_Init();
+	MX_SPI1_Init();
+	/* USER CODE BEGIN 2 */
+	ST7789_Init();
+	Menu();
+	AwaitForAnyButton();
+	SelectDifficulty();
+	/* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+	/* Infinite loop */
+	/* USER CODE BEGIN WHILE */
+	while (1) {
+		char Buttons[4];
+		DetectAnyButtonPress(Buttons);
+		HAL_Delay(1);
+		/* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+		/* USER CODE BEGIN 3 */
+	}
+	/* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void) {
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
-  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+	/** Initializes the RCC Oscillators according to the specified parameters
+	 * in the RCC_OscInitTypeDef structure.
+	 */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI_DIV2;
+	RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL16;
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+	/** Initializes the CPU, AHB and APB buses clocks
+	 */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+			| RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK) {
+		Error_Handler();
+	}
 }
 
 /**
-  * @brief SPI1 Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_SPI1_Init(void)
-{
+ * @brief SPI1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI1_Init(void) {
 
-  /* USER CODE BEGIN SPI1_Init 0 */
+	/* USER CODE BEGIN SPI1_Init 0 */
 
-  /* USER CODE END SPI1_Init 0 */
+	/* USER CODE END SPI1_Init 0 */
 
-  /* USER CODE BEGIN SPI1_Init 1 */
+	/* USER CODE BEGIN SPI1_Init 1 */
 
-  /* USER CODE END SPI1_Init 1 */
-  /* SPI1 parameter configuration*/
-  hspi1.Instance = SPI1;
-  hspi1.Init.Mode = SPI_MODE_MASTER;
-  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
-  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
-  hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
-  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
-  hspi1.Init.NSS = SPI_NSS_SOFT;
-  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
-  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
-  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
-  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
-  hspi1.Init.CRCPolynomial = 10;
-  if (HAL_SPI_Init(&hspi1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  /* USER CODE BEGIN SPI1_Init 2 */
+	/* USER CODE END SPI1_Init 1 */
+	/* SPI1 parameter configuration*/
+	hspi1.Instance = SPI1;
+	hspi1.Init.Mode = SPI_MODE_MASTER;
+	hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+	hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+	hspi1.Init.CLKPolarity = SPI_POLARITY_HIGH;
+	hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+	hspi1.Init.NSS = SPI_NSS_SOFT;
+	hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+	hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+	hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+	hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+	hspi1.Init.CRCPolynomial = 10;
+	if (HAL_SPI_Init(&hspi1) != HAL_OK) {
+		Error_Handler();
+	}
+	/* USER CODE BEGIN SPI1_Init 2 */
 
-  /* USER CODE END SPI1_Init 2 */
+	/* USER CODE END SPI1_Init 2 */
 
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
-static void MX_GPIO_Init(void)
-{
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void) {
+	GPIO_InitTypeDef GPIO_InitStruct = { 0 };
 
-  /* GPIO Ports Clock Enable */
-  __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOB_CLK_ENABLE();
+	/* GPIO Ports Clock Enable */
+	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(ST7789_CS_GPIO_Port, ST7789_CS_Pin, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(ST7789_CS_GPIO_Port, ST7789_CS_Pin, GPIO_PIN_RESET);
 
-  /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, ST7789_DC_Pin|ST7789_RST_Pin|GPIO_PIN_3, GPIO_PIN_RESET);
+	/*Configure GPIO pin Output Level */
+	HAL_GPIO_WritePin(GPIOB, ST7789_DC_Pin | ST7789_RST_Pin | GPIO_PIN_3,
+			GPIO_PIN_RESET);
 
-  /*Configure GPIO pin : ST7789_CS_Pin */
-  GPIO_InitStruct.Pin = ST7789_CS_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(ST7789_CS_GPIO_Port, &GPIO_InitStruct);
+	/*Configure GPIO pin : ST7789_CS_Pin */
+	GPIO_InitStruct.Pin = ST7789_CS_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(ST7789_CS_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : ST7789_DC_Pin ST7789_RST_Pin PB3 */
-  GPIO_InitStruct.Pin = ST7789_DC_Pin|ST7789_RST_Pin|GPIO_PIN_3;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	/*Configure GPIO pins : ST7789_DC_Pin ST7789_RST_Pin PB3 */
+	GPIO_InitStruct.Pin = ST7789_DC_Pin | ST7789_RST_Pin | GPIO_PIN_3;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PA9 PA10 PA11 PA12 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_PULLUP;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+	/*Configure GPIO pins : PA9 PA10 PA11 PA12 */
+	GPIO_InitStruct.Pin = GPIO_PIN_9 | GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_PULLUP;
+	HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 }
 
 /* USER CODE BEGIN 4 */
 
-void Menu(){
+void Menu() {
 	ST7789_WriteString(90, 60, "MENU", Font_16x26, WHITE, BLACK);
 	ST7789_WriteString(60, 120, "New Game", Font_11x18, WHITE, BLACK);
-	char * result;
+	char *result;
 	sprintf(result, "Record: %i", record);
 	ST7789_WriteString(60, 140, result, Font_11x18, WHITE, BLACK);
 
-	ST7789_WriteString(36, 180, "Pressione qualquer botao", Font_7x10, WHITE, BLACK);
+	ST7789_WriteString(36, 180, "Pressione qualquer botao", Font_7x10, WHITE,
+	BLACK);
 	ST7789_WriteString(82, 190, "Pra comecar", Font_7x10, WHITE, BLACK);
 }
-void PrintSelectDifficulty(char SelectedColor = 0){
+void PrintSelectDifficulty(char SelectedColor) {
 	ST7789_Fill_Color(BLACK);
-	ST7789_WriteString(20, 30, "Selecione uma\ndificuldade:", Font_11x18, WHITE, BLACK);
+	ST7789_WriteString(20, 30, "Selecione uma\ndificuldade:", Font_11x18, WHITE,
+	BLACK);
 
 	switch (SelectedColor) {
-		case 0:
-			ST7789_WriteString(10, 120, "- 4x4", Font_11x18, WHITE, BLACK);
-			ST7789_WriteString(10, 140, "- 6x6", Font_11x18, WHITE, BLACK);
-			break;
-		case 1:
-			ST7789_WriteString(10, 120, "- 4x4", Font_11x18, YELLOW, BLACK);
-			ST7789_WriteString(10, 140, "- 6x6", Font_11x18, WHITE, BLACK);
-			break;
-		case 2:
-			ST7789_WriteString(10, 120, "- 4x4", Font_11x18, WHITE, BLACK);
-			ST7789_WriteString(10, 140, "- 6x6", Font_11x18, YELLOW, BLACK);
-			break;
-		default:
-			break;
+	case 0:
+		ST7789_WriteString(10, 120, "- 4x4", Font_11x18, WHITE, BLACK);
+		ST7789_WriteString(10, 140, "- 6x6", Font_11x18, WHITE, BLACK);
+		break;
+	case 1:
+		ST7789_WriteString(10, 120, "- 4x4", Font_11x18, YELLOW, BLACK);
+		ST7789_WriteString(10, 140, "- 6x6", Font_11x18, WHITE, BLACK);
+		break;
+	case 2:
+		ST7789_WriteString(10, 120, "- 4x4", Font_11x18, WHITE, BLACK);
+		ST7789_WriteString(10, 140, "- 6x6", Font_11x18, YELLOW, BLACK);
+		break;
+	default:
+		break;
 	}
 
 }
-void SelectDifficulty(void){
+void SelectDifficulty(void) {
 	PrintSelectDifficulty(0);
 
-
-}
-void AwaitForAnyButton(void)
-{
-	while  (HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9 ) != 0 &&
-			HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10) != 0 &&
-			HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11) != 0 &&
-			HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12) != 0);
-	{
-	}
 }
 
-void IniciarJogo(void){
+void IniciarJogo(void) {
 
 }
-void GerarParesAleatorios(void){
+void GerarParesAleatorios(void) {
 
 }
-void NavegarCursor(void){
+void NavegarCursor(void) {
 
 }
-void SelecionarCarta(void){
+void SelecionarCarta(void) {
 
 }
-void CompararPares(void){
+void CompararPares(void) {
 
 }
-void VerificarFimDeJogo(void){
+void VerificarFimDeJogo(void) {
 
 }
-void AtualizarTentativas(void){
+void AtualizarTentativas(void) {
 
 }
-int AtualizarRecorde(void){
+int AtualizarRecorde(void) {
 
 }
-void ExibirFimDeJogo(void){
+void ExibirFimDeJogo(void) {
 
 }
-char DetectButtonPress(uint16_t Buttons[]){
+void ReadButtons(char *out) {
+	out[0] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9);
+	out[1] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_10);
+	out[2] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_11);
+	out[3] = HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_12);
+}
+void AwaitForAnyButton(void) {
+	char buttons[4];
+
+	do {
+		ReadButtons(buttons);
+	} while (!Contain(buttons, 0));
+}
+void DetectAnyButtonPress(char *out) {
+	char buttons[4];
+	long btnPressTime = 0;
+	int time = 0;
+
+	for (int i = 0; i < 4; ++i)
+		out[i] = 1;
+
+	do {
+		ReadButtons(buttons);
+	} while (!Contain(buttons, 0));
+
+	btnPressTime = HAL_GetTick();
+
+	do {
+		for (int i = 0; i < 4; ++i)
+			if (out[i] != 0)
+				out[i] = buttons[i];
+
+		time = HAL_GetTick() - btnPressTime;
+		ReadButtons(buttons);
+	} while (time < 100);
+}
+void DetectButtonPress(uint16_t buttons[], char *out) {
 	bool botao;
+}
+
+char Contain(char *Iterable, char Contains) {
+	int IterSize = sizeof(Iterable) / sizeof(char);
+	for (int i = 0; i < IterSize; i++) {
+		if (Iterable[i] == Contains) {
+			return 1;
+		}
+	}
+	return 0;
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void) {
+	/* USER CODE BEGIN Error_Handler_Debug */
+	/* User can add his own implementation to report the HAL error return state */
+	__disable_irq();
+	while (1) {
+	}
+	/* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
